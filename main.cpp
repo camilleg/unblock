13acc3869cbc8ecf255b4167bcfaa1893a1f772f
Camille Goudeseune <cog@illinois.edu>
2021-01-31 12:00:50 -0600

    privately store PNG pixels so PNG saves without timestamp

--- main.cpp
+++ main.cpp
@@ -9,38 +9,45 @@ extern "C" {
 #include <algorithm>
 #include <cstdlib>
 
-struct rgbpixel {unsigned char r,g,b; };
+typedef unsigned char u8;
 
 // Actually YCbCr, not YUV.
 // From www.equasys.de/colorconversion.html YCbCr - RGB.
-// (It might also work to use R,G,B directly, each pretending to be Y of YUV.)
-void YUVfromRGB(double& Y, double& U, double& V, const double R, const double G, const double B)
+void YUVfromRGB(u8& Y, u8& U, u8& V, const double R, const double G, const double B)
 {
-  Y =  0.257 * R + 0.504 * G + 0.098 * B +  16;
-  U = -0.148 * R - 0.291 * G + 0.439 * B + 128;
-  V =  0.439 * R - 0.368 * G - 0.071 * B + 128;
+  const double y =  0.257 * R + 0.504 * G + 0.098 * B +  16;
+  const double u = -0.148 * R - 0.291 * G + 0.439 * B + 128;
+  const double v =  0.439 * R - 0.368 * G - 0.071 * B + 128;
+  // Clamp to [0, 255].
+  Y = COSTELLA_IMAGE_LIMIT_RANGE(y);
+  U = COSTELLA_IMAGE_LIMIT_RANGE(u);
+  V = COSTELLA_IMAGE_LIMIT_RANGE(v);
 }
-void RGBfromYUV(double& R, double& G, double& B, double Y, double U, double V)
+void RGBfromYUV(u8& R, u8& G, u8& B, double Y, double U, double V)
 {
   Y -= 16;
   U -= 128;
   V -= 128;
-  R = 1.164 * Y             + 1.596 * V;
-  G = 1.164 * Y - 0.392 * U - 0.813 * V;
-  B = 1.164 * Y + 2.017 * U;
+  const double r = 1.164 * Y             + 1.596 * V;
+  const double g = 1.164 * Y - 0.392 * U - 0.813 * V;
+  const double b = 1.164 * Y + 2.017 * U;
+  // Clamp to [0, 255].
+  R = COSTELLA_IMAGE_LIMIT_RANGE(r);
+  G = COSTELLA_IMAGE_LIMIT_RANGE(g);
+  B = COSTELLA_IMAGE_LIMIT_RANGE(b);
 }
 
 std::string filenameExtension(const std::string& s)
 {
-  const std::string::size_type i = s.find_last_of(".");
+  const auto i = s.find_last_of(".");
   if (i == std::string::npos)
     return "";
-  std::string ext(s.substr(i + 1));
+  auto ext(s.substr(i + 1));
   std::transform(ext.begin(), ext.end(), ext.begin(), ::tolower);
   return ext;
 }
 
-int main( int argc, char** argv)
+int main(int argc, char** argv)
 {
   if (argc != 3) {
     printf("usage: %s in.[bmp|png] out.[bmp|png]\n", argv[0]);
@@ -48,69 +55,67 @@ int main( int argc, char** argv)
   }
 
   if (filenameExtension(argv[1]) != filenameExtension(argv[2]))
-    printf("%s: warning: filenames %s and %s have different extensions.\n", argv[0], argv[1], argv[2]);
-  const bool fPNG = filenameExtension(argv[1]) == "png";
+    printf("%s: warning: filenames %s and %s have different extensions.\nFile %s will get the same format as %s.\n", argv[0], argv[1], argv[2], argv[2], argv[1]);
+  const bool fBMP = filenameExtension(argv[1]) == "bmp";
 
-  int w, h;
   FILE *fp;
-  struct rgbpixel **ppRowPNG = NULL;
-  png_structp pPNG;
-  png_infop pInfoPNG;
+  unsigned w, h;
   BMP bmp;
-  if (fPNG) {
-    fp = fopen(argv[1], "rb");
-    pPNG = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-    pInfoPNG = png_create_info_struct(pPNG);
-    png_init_io(pPNG, fp);
-    png_read_png(pPNG, pInfoPNG, PNG_TRANSFORM_STRIP_ALPHA|PNG_TRANSFORM_EXPAND|PNG_TRANSFORM_STRIP_16, NULL);
-    ppRowPNG = (struct rgbpixel**)png_get_rows(pPNG, pInfoPNG);
-    w = png_get_image_width(pPNG, pInfoPNG);
-    h = png_get_image_height(pPNG, pInfoPNG);
-    png_destroy_read_struct(&pPNG, NULL, NULL);
-    fclose(fp);
-  } else {
+  png_bytep *pRows = NULL;
+
+  if (fBMP) {
     bmp.ReadFromFile(argv[1]);
     w = bmp.TellWidth();
     h = bmp.TellHeight();
+  } else {
+    fp = fopen(argv[1], "rb");
+    auto pPNG = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    auto pInfoPNG = png_create_info_struct(pPNG);
+    png_init_io(pPNG, fp);
+    png_read_info(pPNG, pInfoPNG);
+    if (png_get_color_type(pPNG, pInfoPNG) != PNG_COLOR_TYPE_RGB) {
+      // Alpha, grayscale, and palette formats wouldn't have come from a JPG.
+      printf("%s: png file %s isn't in RGB format.\n", argv[0], argv[1]);
+      return 1;
+    }
+    // Transform 16 bit to 8 bit.
+    if (png_get_bit_depth(pPNG, pInfoPNG) == 16) {
+      png_set_strip_16(pPNG);
+      png_read_update_info(pPNG, pInfoPNG);
+    }
+    w = png_get_image_width(pPNG, pInfoPNG);
+    h = png_get_image_height(pPNG, pInfoPNG);
+    pRows = new png_bytep[h];
+    const auto cb = png_get_rowbytes(pPNG, pInfoPNG);
+    for (unsigned y = 0; y < h; ++y)
+      pRows[y] = new png_byte[cb];
+    png_read_image(pPNG, pRows);
+    fclose(fp);
+    png_destroy_read_struct(&pPNG, &pInfoPNG, NULL);
   }
 
-  // Convert bmp.rgb_data or ppRowPNG to YUV color planes.
-  const unsigned cb = w * h;
-  unsigned char bufY[cb];
-  unsigned char bufU[cb];
-  unsigned char bufV[cb];
-
-  int y,x;
-  int i=0;
+  // Convert bmp.rgb_data or pRows to YUV color planes.
+  const auto cb = w * h;
+  u8 bufY[cb];
+  u8 bufU[cb];
+  u8 bufV[cb];
+  unsigned y, x;
+  auto i = 0;
   for (y = 0; y < h; ++y) {
     for (x = 0; x < w; ++x,++i) {
       double R,G,B;
-      if (fPNG) {
-	const rgbpixel rgb = ppRowPNG[y][x];
-	R = rgb.r;
-	G = rgb.g;
-	B = rgb.b;
-      } else {
-	const RGBApixel* rgb = bmp(x,y);
+      if (fBMP) {
+	const auto rgb = bmp(x,y);
 	R = rgb->Red;
 	G = rgb->Green;
 	B = rgb->Blue;
+      } else {
+	const auto rgb = pRows[y] + 3*x;
+	R = rgb[0];
+	G = rgb[1];
+	B = rgb[2];
       }
-      double Y,U,V;
-      YUVfromRGB(Y,U,V,R,G,B);
-
-#undef testing
-#ifdef testing
-      double a,b,c;
-      RGBfromYUV(a,b,c,Y,U,V);
-      if (fabs(a-R) + fabs(b-G) + fabs(c-B) > 0.9 /* in the scale 0.0 to 255.0 */ )
-	printf("not inverses.\n");
-      // but that's two linear operators, before the necessary clamping.
-#endif
-      // Clamp to 0..255.
-      bufY[i] = COSTELLA_IMAGE_LIMIT_RANGE(Y);
-      bufU[i] = COSTELLA_IMAGE_LIMIT_RANGE(U);
-      bufV[i] = COSTELLA_IMAGE_LIMIT_RANGE(V);
+      YUVfromRGB(bufY[i],bufU[i],bufV[i], R,G,B);
     }
   }
 
@@ -134,12 +139,12 @@ int main( int argc, char** argv)
 #endif
 
   costella_unblock_initialize(stdout);
-  const int fPhoto = 0; // API docs suggest 1, but that boosts ringing of high-contrast detail (timestamps, windows of buildings).
+  const auto fPhoto = 0; // API docs suggest 1, but that boosts ringing of high-contrast detail (timestamps, windows of buildings).
   // (Internal mucking about, in costella_unblock.c bConservativePhotographic tweaking udCumMeasuredConservative,
   // had either no effect or caused a segfault.)
-  int ok = costella_unblock(&im, &im, fPhoto, 0, NULL, NULL, 0);
+  auto ok = costella_unblock(&im, &im, fPhoto, 0, NULL, NULL, 0);
   if (!ok)
-    printf("%s: problem during costella_unblock().\n", argv[0]);
+    printf("%s: costella_unblock() failed.\n", argv[0]);
   costella_unblock_finalize(stdout);
 
   // Convert bufY, bufU, bufV back into a bmp.
@@ -147,34 +152,39 @@ int main( int argc, char** argv)
   // which causes calls to CostellaImageConvertRgbToYcbcr() and CostellaImageConvertYcbcrToRgb().
   // Those call macros like COSTELLA_IMAGE_CONVERT_RGB_TO_YCBCR(),
   // which lookup tables like gasdCostellaImageConvertRCb[] for Red to Cb.)
-  i=0;
+  i = 0;
   for (y = 0; y < h; ++y) {
     for (x = 0; x < w; ++x,++i) {
-      double R,G,B;
-      RGBfromYUV(R,G,B, bufY[i], bufU[i], bufV[i]);
-      if (fPNG) {
-	ppRowPNG[y][x] = (rgbpixel){
-	  COSTELLA_IMAGE_LIMIT_RANGE(R),
-	  COSTELLA_IMAGE_LIMIT_RANGE(G),
-	  COSTELLA_IMAGE_LIMIT_RANGE(B) };
+      u8 R,G,B;
+      RGBfromYUV(R,G,B, bufY[i],bufU[i],bufV[i]);
+      if (fBMP) {
+	auto rgb = bmp(x,y);
+	rgb->Red   = R;
+	rgb->Green = G;
+	rgb->Blue  = B;
       } else {
-	RGBApixel* rgb = bmp(x,y);
-	rgb->Red   = COSTELLA_IMAGE_LIMIT_RANGE(R);
-	rgb->Green = COSTELLA_IMAGE_LIMIT_RANGE(G);
-	rgb->Blue  = COSTELLA_IMAGE_LIMIT_RANGE(B);
+	auto rgb = pRows[y] + 3*x;
+	rgb[0] = R;
+	rgb[1] = G;
+	rgb[2] = B;
       }
     }
   }
-  if (fPNG) {
-    fp = fopen(argv[2], "wb");
-    pPNG = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-    png_init_io(pPNG, fp);
-    png_set_rows(pPNG, pInfoPNG, (png_bytepp)ppRowPNG);
-    png_write_png(pPNG, pInfoPNG, PNG_TRANSFORM_IDENTITY, NULL);
-    png_destroy_write_struct(&pPNG, &pInfoPNG);
-    fclose(fp);
-  } else {
+  if (fBMP) {
     bmp.WriteToFile(argv[2]);
+  } else {
+    fp = fopen(argv[2], "wb");
+    auto pPNG = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    auto pInfoPNG = png_create_info_struct(pPNG);
+    png_init_io(pPNG, fp);
+    png_set_IHDR(pPNG, pInfoPNG, w, h, 8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+    // Don't call png_set_tIME, so no tIME chunk is written, no %tEXtdate:create, %tEXtdate:modify, gAMA, cHRM, bKGD.
+    // Then the output has no timestamp, so it can be diffed against a known-good test output.
+    png_write_info(pPNG, pInfoPNG);
+    png_write_image(pPNG, pRows);
+    png_write_end(pPNG, NULL);
+    fclose(fp);
+    png_destroy_write_struct(&pPNG, &pInfoPNG);
   }
   return 0;
 }
